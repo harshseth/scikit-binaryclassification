f5f336822a105532001ec4101c7d2f36afc0fe49
Harsh Seth <hseth@searshc.com>
2016-10-27 14:19:16 -0500

    Adding logistic regression code

---
+++ logisticregression.py
@@ -0,0 +1,41 @@
+rom sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import cross_validation
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn import linear_model
+from sklearn import grid_search
+
+#initialize
+data=[]
+target = []
+
+#read file and split data and target labels
+with open("Twitter_Spam_Dataset1.csv", 'r') as inpfile:
+	for line in inpfile:
+		container = line.split(",")
+		data.append(line[0])
+		target.append(line[1].replace("\n",""))
+
+
+vectorizer =   TfidfVectorizer(min_df=1,stop_words="english",ngram_range=(1,2),token_pattern="\w+",analyzer = "word")
+X_title = vectorizer.fit_transform(data)
+Y = target
+
+print X_title[0]
+
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+X_title, Y, test_size=0.1, random_state=4563)
+print "split complete"
+print X_train.shape
+
+
+clf = linear_model.LogisticRegression(C = 0.01)
+clf.fit(X_train,y_train)
+
+
+print"predicting"
+pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test,pred)
+print (accuracy)
