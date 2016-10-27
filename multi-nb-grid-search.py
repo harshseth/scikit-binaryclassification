7f77e8b6c288d8e42424825aaddeeb5df483ee5d
Harsh Seth <hseth@searshc.com>
2016-10-27 14:17:00 -0500

    adding multinomial nb with grid search

---
+++ multi-nb-grid-search.py
@@ -0,0 +1,50 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import cross_validation
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn import grid_search
+
+#initialize
+data=[]
+target = []
+
+#read file and split data and target labels
+with open("Twitter_Spam_Dataset1.csv", 'r') as inpfile:
+	for line in inpfile:
+		container = line.split(",")
+		data.append(line[0])
+		target.append(line[1].replace("\n",""))
+
+
+vectorizer =   TfidfVectorizer(min_df=1,stop_words="english",ngram_range=(1,2),token_pattern="\w+",analyzer = "word")
+X_title = vectorizer.fit_transform(data)
+Y = target
+
+print X_title[0]
+
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+X_title, Y, test_size=0.1, random_state=4563)
+print "split complete"
+print X_train.shape
+
+program_grid= [{'alpha':[0.1,0.001,0.0001,1,10,15,100]}]
+
+
+
+nb = MultinomialNB()
+
+clf= grid_search.GridSearchCV(nb, program_grid,n_jobs =-1)
+
+clf.fit(X_train,y_train)
+print "best score"
+print clf.best_score_
+print "best params"
+print clf.best_params_
+
+
+print"predicting"
+pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test,pred)
+print (accuracy)
